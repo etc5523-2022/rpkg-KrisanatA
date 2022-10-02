34468ed895d7e6698db62344ff99b9252b3569dc
Krisanat Anukarnsakulchularp <kanu0003@student.monash.edu>
2022-10-03 02:04:35 +1100

    adding ranking function and create function that provide the list of available information

---
+++ R/list.R
@@ -0,0 +1,39 @@
+# disease -----------------------------------------------------------------
+
+#' Get a list of available disease
+#'
+#' @export
+disease_list <- function() {
+  unique(dislib::mortality$disease)
+}
+
+
+# country -----------------------------------------------------------------
+
+#' Get a list of available country
+#'
+#' @export
+country_list <- function() {
+  unique(dislib::mortality$country)
+}
+
+
+# year --------------------------------------------------------------------
+
+#' Get a list of available year for the country specified
+#'
+#' @export
+`%>%` <- dplyr::`%>%`
+
+year_list <- function(country = NULL) {
+  if (is.null(country)) {
+    stop("Country input are required")
+  }
+
+  else {
+    country <- mortality %>%
+      filter(country == country)
+
+    unique(country$year)
+  }
+}
---
+++ R/ranking.R
@@ -0,0 +1,22 @@
+#'
+#' @export
+
+`%>%` <- dplyr::`%>%`
+
+ranking <- function(country = NULL, year = NULL) {
+  if (is.null(country) | is.null(year)) {
+    stop("The country and year input are required for this function.")
+  }
+  else {
+    mortality %>%
+      dplyr::filter(country == country,
+             year == year) %>%
+      dplyr::group_by(disease) %>%
+      dplyr::summarise(Mortality_rate = mean(`mortality_rate`), .groups = "drop") %>%
+      dplyr::arrange(-Mortality_rate) %>%
+      dplyr::select(-Mortality_rate) %>%
+      dplyr::mutate(Rank = paste0(1:21,".")) %>%
+      dplyr::relocate(disease, .after = Rank) %>%
+      head(10)
+  }
+}
